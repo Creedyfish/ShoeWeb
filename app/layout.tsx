be4d28523bc41559a183c20de2c93a7a46f8cfe1
Creedyfish <ielbanbuenaofficial@gmail.com>
2023-11-30 01:53:58 +0800

    corrected head icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,24 @@ export default async function RootLayout({
   return (
     <html lang="en" className={`${raleway.variable} ${inter.variable} `}>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={` bg-slate-800 ${raleway.className}`}>
         <Navbar />
